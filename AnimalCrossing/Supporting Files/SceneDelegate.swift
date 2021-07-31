5f104dc649091b008b080560e87d6a540c39d39d
Anastasiia Kudasheva <mikahary@yandex.ru>
2021-07-31 21:21:57 +0300

    Code were formatted

--- AnimalCrossing/Supporting Files/SceneDelegate.swift
+++ AnimalCrossing/Supporting Files/SceneDelegate.swift
@@ -8,20 +8,20 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    let coreDataManager = CoreDataManager.shared
-    var window: UIWindow?
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
-                    let window = UIWindow(windowScene: scene)
-        let view = LoginAssebmly().build()
-                    window.rootViewController = UINavigationController(rootViewController: view)
-                    self.window = window
-                    window.makeKeyAndVisible()
-    }
-    
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        self.coreDataManager.saveContext()
-    }
+	let coreDataManager = CoreDataManager.shared
+	var window: UIWindow?
+	
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let scene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: scene)
+		let view = LoginAssebmly().build()
+		window.rootViewController = UINavigationController(rootViewController: view)
+		self.window = window
+		window.makeKeyAndVisible()
+	}
+	
+	func sceneDidEnterBackground(_ scene: UIScene) {
+		self.coreDataManager.saveContext()
+	}
 }
 
